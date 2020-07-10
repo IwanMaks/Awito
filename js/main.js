f81ce50f02b37515a7362958edbc45318a1d5a5b
prostochlen <iwan.maksackov@yandex.ru>
2020-07-10 16:27:25 +0300

    New

---
+++ js/main.js
@@ -0,0 +1,115 @@
+'use strict';
+
+const dataBase = JSON.parse(localStorage.getItem('awito')) || [];
+
+const modalAdd = document.querySelector('.modal__add');
+const addAd = document.querySelector('.add__ad');
+const modalClose = document.querySelector('.modal__close');
+const modalBtnSubmit = document.querySelector('.modal__btn-submit');
+const modalSubmit = document.querySelector('.modal__submit');
+const catalog = document.querySelector('.catalog');
+const modalItem = document.querySelector('.modal__item');
+const modalBtnWarning = document.querySelector('.modal__btn-warning');
+const modalFileInput = document.querySelector('.modal__file-input');
+const modalFileBtn = document.querySelector('.modal__file-btn');
+const modalImageAdd = document.querySelector('.modal__image-add');
+
+const textFileBtn = modalFileBtn.textContent;
+const srcModalImage = modalImageAdd.src;
+
+const elementsModalSubmit = [...modalSubmit.elements]
+    .filter(elem => elem.tagName !== 'BUTTON' && elem.type !== 'submit'); 
+
+const infoPhoto = {};
+
+const saveDB = () => localStorage.setItem('awito', JSON.stringify(dataBase));
+
+const cheakForm = () => {
+    const validForm = elementsModalSubmit.every(elem => elem.value);
+    modalBtnSubmit.disabled = !validForm;
+    modalBtnWarning.style.display = validForm ? 'none' : '';
+};
+
+const closeModal = event => {
+    const target = event.target;
+    if (target.classList.contains('modal__close') ||
+        target.classList.contains('modal') ||
+        event.code === 'Escape') {
+            modalAdd.classList.add('hide');
+            modalItem.classList.add('hide');
+            document.removeEventListener('keydown', closeModal);
+            modalSubmit.reset();
+            modalImageAdd.src = srcModalImage;
+            modalFileBtn.textContent = textFileBtn;
+            cheakForm();
+        }
+};
+
+const renderCard = () => {
+    catalog.textContent = '';
+    dataBase.forEach((item, i) => {
+        catalog.insertAdjacentHTML('beforeend', `
+            <li class="card" data-id="${i}">
+                <img class="card__image" src="data:image/jpeg;base64,${item.image}" alt="test">
+                <div class="card__description">
+                    <h3 class="card__header">${item.nameItem}</h3>
+                    <div class="card__price">${item.costItem} ₽</div>
+                </div>
+             </li>
+        `);
+    });
+};
+
+modalFileInput.addEventListener('change', event => {
+    const target = event.target;
+    const reader = new FileReader();
+    const file = target.files[0];
+    infoPhoto.name = file.name;
+    infoPhoto.size = file.size;
+    reader.readAsBinaryString(file);
+    reader.addEventListener('load',event => {
+        if (infoPhoto.size < 200000) {
+            modalFileBtn.textContent = infoPhoto.name;
+            infoPhoto.base64 = btoa(event.target.result);
+            modalImageAdd.src = `data:image/jpeg;base64,${infoPhoto.base64}`;
+        } else {
+            modalFileBtn.textContent = 'Файл не должен превышать 200 Кб';
+            modalFileInput.value = '';
+            cheakForm();
+        }
+        
+    })
+});
+
+modalSubmit.addEventListener('submit', event => {
+    event.preventDefault();
+    const itemObj = {};
+    for (const elem of elementsModalSubmit) {
+        itemObj[elem.name] = elem.value;
+    }
+    itemObj.image = infoPhoto.base64;
+    dataBase.push(itemObj);
+    closeModal({target: modalAdd});
+    saveDB();
+    renderCard();
+});
+
+addAd.addEventListener('click', () => {
+    modalAdd.classList.remove('hide');
+    modalBtnSubmit.disabled = true;
+    document.addEventListener('keydown', closeModal);
+});
+
+catalog.addEventListener('click', event => {
+    const target = event.target;
+    if (target.closest('.card')) {
+        modalItem.classList.remove('hide')
+        document.addEventListener('keydown', closeModal);
+    };
+});
+
+modalSubmit.addEventListener('input', cheakForm);
+modalAdd.addEventListener('click', closeModal);
+modalItem.addEventListener('click', closeModal);
+
+renderCard();
